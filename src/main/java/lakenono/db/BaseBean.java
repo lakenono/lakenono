5c019c39a690c3fa7bbd33cc4551355261b95977
lakenono <lakenono@126.com>
2015-02-26 16:21:26 +0800

    整理代码.. 去掉测试字段数据 增加对private字段的访问权限

--- src/main/java/lakenono/db/BaseBean.java
+++ src/main/java/lakenono/db/BaseBean.java
@@ -3,7 +3,6 @@ package lakenono.db;
 import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 
 import lakenono.core.GlobalComponents;
 import lakenono.db.annotation.DBField;
@@ -12,28 +11,12 @@ import lakenono.db.annotation.DBTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@DBTable(name = "test_base_bean")
-public class BaseBean {
-
+public abstract class BaseBean
+{
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	private String username;
-	private String password;
-
-	@DBField(type = "datetime")
-	private Date createTime;
-
-	@DBField(type = "Text")
-	private String text;
-
-	@DBField(type = "int")
-	private int age;
-
-	@DBField(serialization = false)
-	private String status;
-
-	public void persist() throws IllegalArgumentException, IllegalAccessException, SQLException, InstantiationException {
-
+	public void persist() throws IllegalArgumentException, IllegalAccessException, SQLException, InstantiationException
+	{
 		StringBuilder sql = new StringBuilder();
 
 		String tablename = this.getClass().getAnnotation(DBTable.class).name();
@@ -41,10 +24,14 @@ public class BaseBean {
 
 		Field[] fields = this.getClass().getDeclaredFields(); // 所有字段
 		ArrayList<Object> params = new ArrayList<Object>();// 持久化字段值
-		for (Field field : fields) {
+		for (Field field : fields)
+		{
+			// 设置private访问权限
+			field.setAccessible(true);
 
 			// 排除掉非序列化字段
-			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).serialization()) {
+			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).serialization())
+			{
 				continue;
 			}
 			sql.append("`" + field.getName() + "`");
@@ -56,7 +43,8 @@ public class BaseBean {
 		sql.append(")");
 
 		sql.append(" values(");
-		for (int i = 0; i < params.size(); i++) {
+		for (int i = 0; i < params.size(); i++)
+		{
 			sql.append("?,");
 		}
 		sql.deleteCharAt(sql.length() - 1);
@@ -67,7 +55,8 @@ public class BaseBean {
 		GlobalComponents.db.getRunner().update(sql.toString(), params.toArray());
 	}
 
-	public void buildTable() throws SQLException {
+	public void buildTable() throws SQLException
+	{
 
 		StringBuilder sql = new StringBuilder();
 
@@ -76,15 +65,23 @@ public class BaseBean {
 
 		Field[] fields = this.getClass().getDeclaredFields();
 
-		for (Field field : fields) {
+		for (Field field : fields)
+		{
+			// 设置private访问权限
+			field.setAccessible(true);
+
 			// 排除掉非序列化字段
-			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).serialization()) {
+			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).serialization())
+			{
 				continue;
 			}
 
-			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).type().equals("varchar")) {
+			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).type().equals("varchar"))
+			{
 				sql.append("`" + field.getName() + "` ").append(field.getAnnotation(DBField.class).type() + " NULL");
-			} else {
+			}
+			else
+			{
 				sql.append("`" + field.getName() + "` ").append("varchar(255) NULL");
 			}
 
@@ -92,15 +89,9 @@ public class BaseBean {
 		}
 		sql.delete(sql.length() - 2, sql.length());
 		sql.append(");");
-		
+
 		this.log.info(sql.toString());
 
 		GlobalComponents.db.getRunner().update(sql.toString());
 	}
-
-	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, SQLException, InstantiationException {
-		new BaseBean().buildTable();
-		new BaseBean().persist();
-	}
-
 }
