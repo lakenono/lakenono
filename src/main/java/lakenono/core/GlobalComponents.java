8fc28b3bdf07bbfd1f9e9640d69d6d3b991dcfa0
lakenono <lakenono@126.com>
2015-03-02 14:25:57 +0800

    增加jsoupfetcher全局实例

--- src/main/java/lakenono/core/GlobalComponents.java
+++ src/main/java/lakenono/core/GlobalComponents.java
@@ -1,9 +1,13 @@
 package lakenono.core;
 
 import lakenono.db.DB;
+import lakenono.fetch.JsoupFetch;
 
-public class GlobalComponents {
+public class GlobalComponents
+{
 
 	public static DB db = new DB();
+
+	public static JsoupFetch fetcher = new JsoupFetch();
 
 }
