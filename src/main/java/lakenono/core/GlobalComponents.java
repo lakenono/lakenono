9f2a138078471300e9da20406a522d3f82a81d47
lakenono <lakenono@126.com>
2015-03-07 11:43:57 +0800

    公共组件加入动态fetcher实例

--- src/main/java/lakenono/core/GlobalComponents.java
+++ src/main/java/lakenono/core/GlobalComponents.java
@@ -1,6 +1,7 @@
 package lakenono.core;
 
 import lakenono.db.DB;
+import lakenono.fetch.DynamicFetch;
 import lakenono.fetch.JsoupFetch;
 
 public class GlobalComponents
@@ -9,5 +10,7 @@ public class GlobalComponents
 	public static DB db = new DB();
 
 	public static JsoupFetch fetcher = new JsoupFetch();
+
+	public static DynamicFetch dynamicFetch = new DynamicFetch();
 
 }
