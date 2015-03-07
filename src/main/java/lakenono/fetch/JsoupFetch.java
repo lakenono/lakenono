20f80d625169a5fc55f60279b15cb60e7f997cd4
lakenono <lakenono@126.com>
2015-03-07 17:37:59 +0800

    增加直接返回document的方法

--- src/main/java/lakenono/fetch/JsoupFetch.java
+++ src/main/java/lakenono/fetch/JsoupFetch.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +14,7 @@ public class JsoupFetch
 
 	private final int retry = 50;
 
-	public String fetch(String url) throws IOException, InterruptedException
+	public Document document(String url) throws IOException, InterruptedException
 	{
 		log.debug("fetch - {}", url);
 
@@ -48,14 +49,19 @@ public class JsoupFetch
 		throw new RuntimeException("JsoupFetch重试[" + retry + "]次后无法成功.");
 	}
 
-	private String connect(String url) throws IOException
+	public String fetch(String url) throws IOException, InterruptedException
+	{
+		return this.document(url).html();
+	}
+
+	private Document connect(String url) throws IOException
 	{
 		Connection connect = Jsoup.connect(url);
 		connect.userAgent("Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
 		connect.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 		connect.timeout(1000 * 10);
 
-		return connect.get().html();
+		return connect.get();
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException
