e764a321f319a8bb45ecf9d0183737a72a4f8b57
lakenono <lakenono@126.com>
2015-03-08 14:42:21 +0800

    基础日志

---
+++ src/main/java/lakenono/log/BaseLog.java
@@ -0,0 +1,9 @@
+package lakenono.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseLog
+{
+	protected final Logger log = LoggerFactory.getLogger(this.getClass());
+}
