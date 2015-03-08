3afba3d02538818ef97bb8609c4f9a0352059843
lakenono <lakenono@126.com>
2015-03-08 14:53:39 +0800

    注册taskservice

--- src/main/java/lakenono/core/GlobalComponents.java
+++ src/main/java/lakenono/core/GlobalComponents.java
@@ -3,7 +3,12 @@ package lakenono.core;
 import lakenono.db.DB;
 import lakenono.fetch.DynamicFetch;
 import lakenono.fetch.JsoupFetch;
+import lakenono.task.TaskService;
 
+/**
+ * @author hu.xinlei
+ *
+ */
 public class GlobalComponents
 {
 
@@ -12,5 +17,7 @@ public class GlobalComponents
 	public static JsoupFetch fetcher = new JsoupFetch();
 
 	public static DynamicFetch dynamicFetch = new DynamicFetch();
+
+	public static TaskService taskService = new TaskService();
 
 }
