9d1ae8002914a96f353fd574554a28ed862678d3
wuhao666 <sky-boy-sunny@126.com>
2016-03-04 14:24:05 +0800

    爬虫动态建表，取数方法

--- src/main/java/lakenono/db/DBBean.java
+++ src/main/java/lakenono/db/DBBean.java
@@ -4,9 +4,13 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import lakenono.core.GlobalComponents;
+import lakenono.db.annotation.Column;
 import lakenono.db.annotation.DBConstraintPK;
 import lakenono.db.annotation.DBField;
 import lakenono.db.annotation.DBTable;
@@ -16,7 +20,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.apache.commons.lang3.StringUtils;
+
+import com.alibaba.fastjson.JSON;
 
 /**
  * 数据库访问层
@@ -274,8 +282,71 @@ public class DBBean {
 			return false;
 		}
 	}
+	
+	public boolean saveOnNotExist(String tableKey) throws IllegalArgumentException, IllegalAccessException, SQLException {
+		StringBuilder sql = new StringBuilder();
 
+		String tableName = getTableName(tableKey);
+		sql.append("INSERT IGNORE INTO `" + tableName + "`(");
+
+		Field[] fields = this.getClass().getDeclaredFields(); // 所有字段
+		ArrayList<Object> params = new ArrayList<Object>();// 持久化字段值
+
+		for (Field field : fields) {
+			// 设置private访问权限
+			field.setAccessible(true);
+
+			// 排除掉非序列化字段
+			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).serialization()) {
+				continue;
+			}
+
+			// 排除静态变量
+			int mo = field.getModifiers();
+			String priv = Modifier.toString(mo);
+			if (StringUtils.contains(priv, "static")) {
+				continue;
+			}
+
+			sql.append("`" + field.getName() + "`");
+			sql.append(",");
+
+			// value
+			params.add(field.get(this));
+		}
+		sql.deleteCharAt(sql.length() - 1);
+		sql.append(")");
+
+		sql.append(" VALUES(");
+		for (int i = 0; i < params.size(); i++) {
+			sql.append("?,");
+		}
+		sql.deleteCharAt(sql.length() - 1);
+		sql.append(");");
+
+		log.debug("{}", sql.toString());
+
+		int result = GlobalComponents.db.getRunner().update(sql.toString(), params.toArray());
+
+		if (result > 0) {
+			log.debug("{} insert finish ! ", this);
+			return true;
+		} else {
+			log.debug("{} has bean exist ! ", this);
+			return false;
+		}
+	}
+	
+	
 	private String getTableName() {
+		String tableName = this.getClass().getAnnotation(DBTable.class).name();
+		if (StringUtils.isNotBlank(tableKey)) {
+			tableName = tableName + "_" + tableKey;
+		}
+		return tableName;
+	}
+	
+	private String getTableName(String tableKey) {
 		String tableName = this.getClass().getAnnotation(DBTable.class).name();
 		if (StringUtils.isNotBlank(tableKey)) {
 			tableName = tableName + "_" + tableKey;
@@ -294,5 +365,142 @@ public class DBBean {
 		String tablename = getTableName(c);
 		return GlobalComponents.db.getRunner().query("SELECT * FROM " + tablename, new BeanListHandler<T>(c, new BasicRowProcessor()));
 	}
+	
+	/**
+	 * 
+	 * @param dbBean
+	 * @param taskId
+	 * @param map
+	 * @param page
+	 * @param pageSize
+	 * @return
+	 * @throws SQLException
+	 * @author wuhao
+	 */
+	public static<T> String getSelectColumnDatas(Class<?> dbBean,String taskId,Map<String,Object>  searchMap,Map<String, Object> pageMap) throws SQLException{
+		String tableName = getTableName(dbBean) +"_" + taskId;
+		//String tableName = getTableName(dbBean);
+		
+		StringBuilder selectSql = new StringBuilder();
+		selectSql.append("select  ");
+		
+		StringBuilder recordsSql = new StringBuilder();
+		recordsSql.append("select  count(*) from ");
+		recordsSql.append(tableName);
+		
+		List<Map<String,String>> columnList = new ArrayList<Map<String,String>>();
+		
+		Field[] fields = dbBean.getDeclaredFields();// 所有字段
+		
+		for (Field field : fields) {
+			// 设置private访问权限
+			field.setAccessible(true);
 
+			// 排除掉非序列化字段
+			if (field.getAnnotation(DBField.class) != null && !field.getAnnotation(DBField.class).serialization()) {
+				continue;
+			}
+
+			//是否查询字段
+			if (field.getAnnotation(Column.class) != null && !field.getAnnotation(Column.class).selectColumn()) {
+				continue;
+			}
+
+			// 排除静态变量
+			int mo = field.getModifiers();
+			String priv = Modifier.toString(mo);
+			if (StringUtils.contains(priv, "static")) {
+				continue;
+			}
+			
+			//获取查询数据
+			selectSql.append("`" + field.getName() + "`");
+			selectSql.append(",");
+			
+			Map<String,String> columnMap = new HashMap<String,String>();
+			columnMap.put(field.getName(), field.getAnnotation(Column.class).columnAs());
+			columnList.add(columnMap);
+			
+		}
+		
+		selectSql.deleteCharAt(selectSql.length() - 1);
+		selectSql.append(" from " + tableName + "");
+		selectSql.append(" where 1 = 1");
+		
+		
+		if(pageMap != null){
+			int limit1 = (Integer.valueOf(String.valueOf(pageMap.get("currentPage"))) - 1)*Integer.valueOf(String.valueOf(pageMap.get("pageSize")));
+			int limit2 = Integer.valueOf(String.valueOf(pageMap.get("pageSize")));
+			selectSql.append(" limit " + limit1 + ", " + limit2);
+		}
+		
+		Set<String> keys = searchMap.keySet();
+		for(String key :keys){
+		     System.out.println(key+" "+searchMap.get(key));
+		}
+		
+		Map<String,Object> param = new HashMap<String,Object>();
+		param.put("column", columnList);
+		
+		
+		log.debug("{} query dbbean  >  ", selectSql.toString());
+		List<Map<String,Object>> sqllist = (List<Map<String,Object>>)GlobalComponents.db.getRunner().query(selectSql.toString(), new MapListHandler());
+		
+		Object obj = null;
+		obj = GlobalComponents.db.getRunner().query(recordsSql.toString(), new ScalarHandler<Object>());
+		String recordCount = String.valueOf(obj);
+
+		pageMap.put("recordCount", recordCount);
+		
+		param.put("data", sqllist);
+		param.put("page", pageMap);
+		
+		String str = JSON.toJSONString(param);
+		
+		return str;
+	}
+	
+	public String updateTableData(Class<?> dbBean,String taskId,Map<String,String>  map){
+		String tableName = getTableName(dbBean) +"_" + taskId;
+		
+		String id = "";
+		String columnEn = "";
+		String columnValue = "";
+		
+		if(map.get("id") != null && !"".equals(map.get("id")) && map.get("columnEn") != null && !"".equals(map.get("columnEn")) 
+			&& map.get("columnValue") != null && !"".equals(map.get("columnValue"))	){
+			id = String.valueOf(map.get("id"));
+			columnEn = String.valueOf(map.get("id"));
+			columnValue = String.valueOf(map.get("id"));
+		}else{
+			return null;
+		}
+		
+		StringBuilder updateSql = new StringBuilder();
+		updateSql.append(" update ");
+		updateSql.append( tableName + " set  ");
+		updateSql.append(columnEn + " = '" + columnValue + "' ");
+		updateSql.append(" where id = '" + id + "'");
+		
+		int i = 0;
+		try {
+			i = GlobalComponents.db.getRunner().update(updateSql.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return String.valueOf(i);
+	}
+	
+	public String appendTableRemarks(Class<?> dbBean, String taskId, Map<String,String>  map){
+		String tableName = getTableName(dbBean) +"_" + taskId;
+		/**
+		 * @reason 需要对数据 list 做操作！
+		 */
+		
+		
+		return null;
+	}
+	
+	
+	
 }
